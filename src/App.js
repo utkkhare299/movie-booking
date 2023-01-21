d228d391cecce00928f6711f98cae580511f05b0
utkarsh khare <utkkhare999@gmail.com>
2023-01-22 00:57:49 +0530

    Making apps error proof

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-  async function fetchMoviesHandler() {
-    const response = await fetch("https://swapi.dev/api/films/");
-    const data = await response.json();
+  const controller = new AbortController();
+  const signal = controller.signal;
+
+  const onClick = () => controller.abort();
+
+  const fetchMoviesHandler = useCallback(async () => {
+    setIsLoading(true);
+    setError(null);
+    let data = null;
+    try {
+      const response = await fetch("https://swapi.dev/api/films/");
+      if (!response.ok) {
+        throw new Error("Something went wrong! Retrying...");
+      }
+
+      data = await response.json();
+    } catch (error) {
+      setInterval(async () => {
+        setError(error.message);
+        const newResponse = await fetch("https://swapi.dev/api/films/", {
+          signal,
+        });
+        data = await newResponse.json();
+        return data;
+      }, 5000);
+    }
+    setIsLoading(false);
 
     const transformedMovies = data.results.map((movieData) => {
       return {
@@ -19,16 +45,27 @@ function App() {
       };
     });
     setMovies(transformedMovies);
+  }, []);
+  let content = <p>Found no movies.</p>;
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+
+  if (error) {
+    content = <p>{error}</p>;
+  }
+
+  if (isLoading) {
+    content = <p>Loading...</p>;
   }
 
   return (
     <React.Fragment>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+        <button onClick={onClick}>Cancel Request</button>
       </section>
-      <section>
-        <MoviesList movies={movies} />
-      </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
